4a906833e920455392784a5fc935c4e3d3dfb7f3
Valentina Koch <valentinakoch2024@gmail.com>
2026-06-17 18:19:20 -0300

    arreglo para el deploy

--- gestion-tributaria/app/layout.tsx
+++ gestion-tributaria/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider afterSignInUrl="/dashboard" afterSignUpUrl="/dashboard">
+    <ClerkProvider signInFallbackRedirectUrl="/dashboard" signUpFallbackRedirectUrl="/dashboard">
       <html lang="es">
         <body>
           {children}
